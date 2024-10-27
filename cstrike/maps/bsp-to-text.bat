6f90f19a52f8d7ad67d6a03cfcc2b44c8c9d9fd4
Rabtos <124796345+Rabtos@users.noreply.github.com>
2024-10-27 13:36:01 +0100

    Added roadwars map

---
+++ cstrike/maps/bsp-to-text.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+rem Pfad zum Speicherort der Batch-Datei
+set "folder_path=%~dp0"
+
+rem Textdatei, in die die Dateinamen geschrieben werden
+set "output_file=%folder_path%bsp_files.txt"
+
+rem Löscht die Textdatei, falls sie bereits existiert, um Dopplungen zu vermeiden
+if exist "%output_file%" del "%output_file%"
+
+rem Durchsuche alle .bsp-Dateien im Ordner der .bat-Datei
+for %%f in (*.bsp) do (
+    echo %%~nf >> "%output_file%"
+)
+
+echo Alle Dateinamen wurden in %output_file% gespeichert.
+endlocal
